295ab3af8545bc8e17699207eced26b026b92203
andersonspl@hotmail.com <49296068+PrimeiroControle@users.noreply.github.com>
2022-05-30 13:47:15 -0300

    Otimizando e Inserindo Gráficos

--- main.py
+++ main.py
@@ -1,95 +1,106 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-#IMPORTANDO DATAFRAME
-servDf = pd.read_csv('Arquivos\BaseServiçosPrestados.csv', encoding='latin1')
-cliDf = pd.read_csv('Arquivos\CadastroClientes.csv')
-funcDf = pd.read_csv('Arquivos\CadastroFuncionarios.csv', sep=';', decimal=',')
+# IMPORTANDO DATAFRAME
 
-#1 - Qual o valor total da folha salarial da empresa ?
-#Analizando e Calculando os dados de funcionários
+servDf = pd.read_csv(r'Arquivos\BaseServiçosPrestados.csv', encoding='latin1')
+cliDf = pd.read_csv(r'Arquivos\CadastroClientes.csv')
+funcDf = pd.read_csv(r'Arquivos\CadastroFuncionarios.csv', sep=';', decimal=',')
+
+# 1 - Qual o valor total da folha salarial da empresa ?
+# Analizando e Calculando os dados de funcionários
 fSalarial = funcDf['Salario Base'] + funcDf['Impostos'] + funcDf['Beneficios'] + funcDf['VT'] + funcDf['VR']
 fSalarial = fSalarial.sum()
 
 resultado = 'R$ {:_.2f}'.format(fSalarial)
-resultado = resultado.replace('.', ',').replace('_','.')
+resultado = resultado.replace('.', ',').replace('_', '.')
 titulo = '< FOLHA SALARIAL >'
-print('{:═^50}\nTOTAL.: {}\n'.format(titulo,resultado))
+print('{:═^50}\nTOTAL.: {}\n'.format(titulo, resultado))
 
-#2 - Qual foi o faturamento da empresa ?
+# 2 - Qual foi o faturamento da empresa ?
 
-#Puxando o DATAFRAME
+# Puxando o DATAFRAME
 fatDf = servDf
 
-#Mesclando o DATAFRAME
+# Mesclando o DATAFRAME
 fatDf = fatDf.merge(cliDf, on='ID Cliente')
 
-#Calculando os Valores de cada cliente
+# Calculando os Valores de cada cliente
 fatDf['Total Faturamento por Cliente'] = fatDf['Tempo Total de Contrato (Meses)'] * fatDf['Valor Contrato Mensal']
 
-#Somando o faturamento
+# Somando o faturamento
 vContratoTt = fatDf['Total Faturamento por Cliente'].sum()
 
-#Formatação
+# Formatação
 fatTt = 'R$ {:_.2f}'.format(int(vContratoTt))
-fatTt = fatTt.replace('.', ',').replace('_','.')
+fatTt = fatTt.replace('.', ',').replace('_', '.')
 titulo = '< FATURALMENTO DA EMPRESA >'
 
-#Exibindo o Resultado
-print('{:═^50}\nTOTAL.: {}\n'.format(titulo,fatTt))
+# Exibindo o Resultado
+print('{:═^50}\nTOTAL.: {}\n'.format(titulo, fatTt))
 
-#3 - Qual a % de funcionários que fechou contratos ?
+# 3 - Qual a % de funcionários que fechou contratos ?
 
-#Calculando quantos funcionários existem registrados
+# Calculando quantos funcionários existem registrados
 ttFunc = len(funcDf['ID Funcionário'])
 
-#Calculando quantos contratos foram fechados
+# Calculando quantos contratos foram fechados
 contratosFuncTt = fatDf['ID Funcionário'].value_counts()
 contratosFuncTt = len(contratosFuncTt)
-perFuncContratos = contratosFuncTt/ttFunc
+perFuncContratos = contratosFuncTt / ttFunc
 
-#Exibindo os resultados da Analise
-titulo ='< % DE FUNCIONÁRIO COM CONTRATOS REALIZADOS >'
-print('{:═^70}\nVALOR.: {:.2%}\n'.format(titulo,perFuncContratos))
+# Exibindo os resultados da Analise
+titulo = '< % DE FUNCIONÁRIO COM CONTRATOS REALIZADOS >'
+print('{:═^70}\nVALOR.: {:.2%}\n'.format(titulo, perFuncContratos))
 
-#4 - Total de Controtos realizado por Setor
-#Puxando o DATAFRAME
+# 4 - Total de Controtos realizado por Setor
+# Puxando o DATAFRAME
 contArea = servDf
 
-#Criando um novo DATAFRAME apenas com os dados relevantes
+# Criando um novo DATAFRAME apenas com os dados relevantes
 nFuncDf = funcDf[['ID Funcionário', 'Area']]
 
-#Mesclando os DATAFRAMES
+# Mesclando os DATAFRAMES
 contArea = contArea.merge(nFuncDf, on='ID Funcionário')
 
-#Realizando o calculo de contratos por Área
-areaCount = contArea['Area'].value_counts()
+# Realizando o calculo de contratos por Área
+areaCount = dict(contArea['Area'].value_counts())
 
-#Exibindo os dados da Analise (Valor e Gráfico)
-titulo ='< TOTAL DE CONTRATOS REALIZADOS POR SETOR >'
+# Exibindo os dados da Analise (Valor e Gráfico)
+titulo = '< TOTAL DE CONTRATOS REALIZADOS POR SETOR >'
 print('{:═^70}\n{}\n'.format(titulo,areaCount))
-#areaCount.plot(kind='bar')
+setor = areaCount.keys()
+value = areaCount.values()
+plt.bar(setor, value)
+plt.show()
+# 5 - Quantidade de Funcionários por Setor
 
-#5 - Quantidade de Funcionários por Setor
-
-#Criando um novo DATAFRAME
+# Criando um novo DATAFRAME
 qntdFunc = funcDf
 
-#Calculando a quantidade de funcionário por Área
-qntdFunc = qntdFunc['Area'].value_counts()
+# Calculando a quantidade de funcionário por Área
+qntdFunc = dict(qntdFunc['Area'].value_counts())
 
-#Exibindo os dados Analisados (Tabela e Gráfico)
-print(qntdFunc)
-#qntdFunc.plot(kind='bar')
+# TRANSFORMANDO O RESULTADO EM DICIONÁRIO E CONVERTENDO EM LISTA
+func = qntdFunc.keys()
+value = qntdFunc.values()
 
-#6 Qual o ticket médio da empresa mensal ?
+# Exibindo os dados Analisados (Tabela e Gráfico)
+titulo = '< QUANTIDADE DE FUNCIONÁRIOS POR SETOR >'
 
-#Calculando o ticket médio mensal
+print('{:═^70}\n{}\n'.format(titulo, qntdFunc))
+plt.bar(func, value)
+plt.show()
+
+# 6 Qual o ticket médio da empresa mensal ?
+
+# Calculando o ticket médio mensal
 ticketMedio = cliDf['Valor Contrato Mensal'].mean()
 
-#FORMATANDO O RESULTADO DA MÉDIA DE VALORES DOS CONTRATOS
+# FORMATANDO O RESULTADO DA MÉDIA DE VALORES DOS CONTRATOS
 ticketMedio = 'R$ {:_.2f}'.format(ticketMedio)
-ticketMedio = ticketMedio.replace('.',',').replace('_','.')
+ticketMedio = ticketMedio.replace('.', ',').replace('_', '.')
 
-#IMMPRIMINDO RESULTADO
+# IMMPRIMINDO RESULTADO
 titulo = '< TICKET MÉDIO DA EMPRESA (MENSAL)>'
 print('{:═^50}\nTOTAL.: {}'.format(titulo, ticketMedio))
